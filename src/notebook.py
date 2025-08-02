a429477239d2f832d6cc568cce70bd1fab3573d2
Akayou Adane Kitessa <38943947+akuadane@users.noreply.github.com>
2025-08-01 22:33:57 -0400

    Implemented session tracking with files (#3)

--- src/notebook.py
+++ src/notebook.py
@@ -1,22 +1,33 @@
 from jupyter_client import KernelManager
 from jupyter_client.blocking.client import BlockingKernelClient
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+
+parent_folder  = Path(__file__).resolve().parent
+env_path =  parent_folder / '.env'
+
+load_dotenv(dotenv_path=env_path)
+notebooks_folder_name = os.getenv('NOTEBOOKS_FOLDER')
+notebooks_folder = parent_folder / notebooks_folder_name
 
 class Notebook:
-    def __init__(self, session_id):
+    def __init__(self,session_id):
         self.kernel =  KernelManager()
         self.kernel.start_kernel()
-        
+        self.session_id = session_id
         self.client = BlockingKernelClient(connection_file=self.kernel.connection_file)
         self.client.load_connection_file()
         self.client.start_channels()
         self.client.wait_for_ready(timeout=5) 
 
-        self.session_id = session_id
+        self.file_path = os.path.join(notebooks_folder, f'{self.session_id}.txt')
 
         self.history = []
     
     def execute_new_code(self,code):
-        self.history.append(code)
+        
         result = []
         error = []
         def output_callback(msg):
@@ -33,21 +44,23 @@ class Notebook:
             output_hook=output_callback,
             stop_on_error=True  # Optional: stop if an error occurs
         )       
-       
+        if len(error)==0:
+            self.history.append('\n'+code)
         return {"error": error, "result":result}
     
     def dump_to_file(self):
-        with open(f'{self.session_id}.txt','w') as f:
+        with open(self.file_path,'w') as f:
             for code in self.history:
                 f.write(code+'\n')
 
     def load_from_file(self):
         try:
-            with open(f'{self.session_id}.txt') as f:
+            with open(self.file_path) as f:
                 code = f.read()
                 self.execute_new_code(code)
+                return True
         except:
-            print("Can't open file.")
+            return False
 
     # TODO abstract out creating a new client
     def close(self):
--- src/server.py
+++ src/server.py
@@ -1,6 +1,16 @@
 import time
 from mcp.server.fastmcp import FastMCP, Context
 from notebook import Notebook
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+parent_folder  = Path(__file__).resolve().parent
+env_path =  parent_folder / '.env'
+
+load_dotenv(dotenv_path=env_path)
+notebooks_folder_name = os.getenv('NOTEBOOKS_FOLDER')
+notebooks_folder = parent_folder / notebooks_folder_name
 
 
 mcp = FastMCP(name = 'Code Interpreter", "2.0.0", "A simple code interpreter that executes Python code and returns the result.',
@@ -10,11 +20,22 @@ mcp = FastMCP(name = 'Code Interpreter", "2.0.0", "A simple code interpreter tha
                 It is crucial to remember your session_id for a smooth interaction.
                 ```
                 )""")
-notebook = None
+sessions = {}
 
-@mcp.tool('execute_code')
+@mcp.tool('execute_code', description='''Executes the provided Python code, saves code for future use and returns the result.
+    
+    Args:
+        code (str): The Python code to execute.
+        session_id (int, optional): A unique identifier used to associate multiple code execution requests
+            with the same logical session. If this is the first request, you may omit it or set it to 0.
+            The system will generate and return a new session_id, which should be reused in follow-up requests
+            to maintain continuity within the same session. You don't need to repeat previous code to access it, just use the variables define earlier.
+    
+    Returns:
+        dict: A dictionary containing the result or an error message.''' )
+
 async def execute_code(code: str,ctx: Context, session_id: int = 0) -> dict:
-    global notebook
+    global sessions
     """
     Executes the provided Python code and returns the result.
     
@@ -29,19 +50,26 @@ async def execute_code(code: str,ctx: Context, session_id: int = 0) -> dict:
         dict: A dictionary containing the result or an error message.
     """
     session_info = None
-    if session_id==0 or (notebook and notebook.session_id!=session_id):
+
+    if session_id==0 or not os.path.exists(os.path.join(notebooks_folder, f'{session_id}.txt')):
         session_id = int(time.time())
-        notebook = Notebook(session_id)
+        sessions[session_id] =  Notebook(session_id)
         session_info = f"Your session_id for this chat is {session_id}. You should provide it for subsequent requests."
+    elif not sessions.get(session_id):
+        notebook = Notebook(session_id)
+        notebook.load_from_file()
+        sessions[session_id] = notebook
 
     try:
+        notebook = sessions[session_id]
         result = notebook.execute_new_code(code)
         if session_info:
             result['result'].append(session_info)
-        
+        if len(result['error'])==0:
+            notebook.dump_to_file()
         return result 
     except Exception as e:
-        return {'error':str(e), 'result':[] }
+        return {'error':[str(e)], 'result':[] }
 
 
 
